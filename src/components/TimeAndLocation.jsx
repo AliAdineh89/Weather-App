29341867b8e57382fe6a65881519a71ecc84e701
Ali Adineh <ahoora.ali@gmx.at>
2023-08-14 16:39:35 +0200

    hard coding

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+      <div className=" flex items-center justify-center my-6">
+        <p className=" text-white text-xl font-extralight">
+          Monday, 14 August 2023 | Local time: 03:50 PM
+        </p>
+      </div>
+      <div className=" flex items-center justify-center my-3">
+        <p className=" text-white text-3xl font-medium">Wien, AT</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLocation;
